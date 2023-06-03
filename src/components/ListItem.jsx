bd194ac04ec1301b2897a1d8dccb3983dbfca4da
Andre Mello <amellow@Andres-MacBook-Air.local>
2023-06-03 17:33:13 -0400

    refactored and added animations

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,8 +4,8 @@ import typeWrite from "../scripts/typeWrite";
 export default function ListItem({
   url,
   new_tab = null,
-  img_src = "/lambda.png",
-  img_alt = "lambda",
+  img_src = null,
+  img_alt = null,
   title,
   tags,
   speed = 50,
@@ -19,20 +19,20 @@ export default function ListItem({
 
   setTimeout(() => {
     typeWrite(tags, set_tags, speed);
-  }, delay * 2 + 800);
+  }, delay + 600);
 
   return (
-    <li class="flex justify-center mb-6 font-mono min-h-[200px] hover:scale-105 transition-transform">
+    <li class="flex justify-center mt-6 font-mono min-h-[200px] hover:scale-105 transition-transform">
       <a class="" href={url} target={new_tab ? "_blank" : "_self"}>
         <div class="flex flex-col sm:flex-row w-[280px] sm:w-[580px] md:w-[700px] lg:w-[800px] border-b-2 border-gray">
-          <img
-            src={img_src}
-            alt={img_alt}
-            class=" border-black border-2 sm:mx-6 mb-5 mx-auto"
-            height="180px"
-            width="180px"
-          />
-          <div class="h-[100%] px-6 min-h-[120px] min-w-[160px] text-center sm:text-left">
+          {img_src && (
+            <img
+              src={img_src}
+              alt={img_alt}
+              class="sm:mx-6 mb-5 mx-auto object-contain w-[180px] h-[180px]"
+            />
+          )}
+          <div class="h-[100%] px-6 min-h-[180px] min-w-[160px] text-center sm:text-left">
             <h2 class="">{curr_title()}</h2>
             <p class="font-thin mt-4">{curr_tags()}</p>
           </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,31 @@
 import { createSignal } from "solid-js";
 import ListItem from "./ListItem";
 export default function Search({ items }) {
+  items = items.sort();
   const uniqueTags = [
+    "All",
     ...new Set(items.map((item) => item.frontmatter.tags).flat()),
   ];
-  const [curr_items, set_items] = createSignal(items);
+  const [curr_items, set_items] = createSignal(items.sort());
   const num_items = items.length;
 
   return (
     <>
-      {num_items >= 2 && (
-        <div className="flex flex-row flex-wrap w-[280px] sm:w-[580px] md:w-[700px] lg:w-[800px] mx-auto justify-around px-6 py-10 font-mono">
-          {uniqueTags.map((tag) => (
+      {num_items >= 5 && (
+        <div className="flex flex-row flex-wrap w-[280px] sm:w-[580px] md:w-[700px] lg:w-[800px] mx-auto px-6 pb-10 font-mono">
+          {uniqueTags.sort().map((tag) => (
             <button
-              className="inline-block p-2 m-1 rounded-3xl bg-gray-200 hover:scale-110 hover:shadow-xl transition-all ease-in-out"
+              className={
+                "inline-block p-2 m-1 rounded-3xl bg-gray-200 hover:scale-110 hover:shadow-xl transition-all ease-in-out"
+              }
               onClick={() => {
-                set_items(
-                  items.filter((item) => item.frontmatter.tags.includes(tag))
-                );
+                if (tag != "All") {
+                  set_items(
+                    items.filter((item) => item.frontmatter.tags.includes(tag))
+                  );
+                } else {
+                  set_items(items);
+                }
               }}
             >
               {tag}
@@ -29,9 +37,10 @@ export default function Search({ items }) {
         <ListItem
           title={item.frontmatter.title}
           url={item.frontmatter.url ?? item.url}
-          tags={item.frontmatter.tags.join(", ")}
+          img_src={item.frontmatter.image.url}
+          tags={item.frontmatter.tags.sort().join(", ")}
           new_tab={item.frontmatter.new_tab}
-          delay={index * 300}
+          delay={index * 200}
         />
       ))}
     </>
