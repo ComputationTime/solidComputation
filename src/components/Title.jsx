132951c66e2b5ca73cdea5cd35cc09138af9a8e5
Andre Mello <amellow@Andres-MacBook-Air.local>
2023-06-04 17:22:04 -0400

    centered title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,10 +5,12 @@ export default function Title({ title }) {
   const [writing, setWriting] = createSignal("");
   typeWrite(title, setWriting, 8);
   return (
-    <>
-      <h1 className="text-5xl sm:text-7xl text-center font-mono font-thin mt-[240px] after:content-[''] after:inline-block after:translate-y-[4px] after:w-[30px] after:h-[60px] after:bg-black after:opacity-1 after:animate-fade_in_out">
-        {writing()}
-      </h1>
-    </>
+    <div className="table-row justify-center h-full">
+      <div className="table-cell align-middle">
+        <h1 className="align-middle mb-28 leading-10 text-5xl sm:text-7xl text-center font-mono font-thin after:content-[''] after:inline-block after:translate-y-[4px] after:w-[30px] after:h-[60px] after:bg-black after:opacity-1 after:animate-fade_in_out">
+          {writing()}
+        </h1>
+      </div>
+    </div>
   );
 }
