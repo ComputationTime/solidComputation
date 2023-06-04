a2652ec39fb45bca4265315131e19252e6ab65e5
Andre Mello <amellow@Andres-MacBook-Air.local>
2023-06-04 19:22:59 -0400

    centered title a bit different

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,7 +7,7 @@ export default function Title({ title }) {
   return (
     <div className="table-row justify-center h-full">
       <div className="table-cell align-middle">
-        <h1 className="align-middle mb-28 leading-10 text-5xl sm:text-7xl text-center font-mono font-thin after:content-[''] after:inline-block after:translate-y-[4px] after:w-[30px] after:h-[60px] after:bg-black after:opacity-1 after:animate-fade_in_out">
+        <h1 className="align-middle mb-36 leading-10 text-5xl sm:text-7xl text-center font-mono font-thin after:content-[''] after:inline-block after:translate-y-[4px] after:w-[30px] after:h-[60px] after:bg-black after:opacity-1 after:animate-fade_in_out">
           {writing()}
         </h1>
       </div>
