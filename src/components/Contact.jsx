cd961e060039f6009219df7a3f9afd0d30c3aa67
Andre Mello <amellow@Andres-MacBook-Air.local>
2023-06-28 08:44:53 -0400

    fixed unhighlightable contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,10 +18,10 @@ export default function Contact() {
             set_phone(part_1 + part_2 + (num_1 + num_2));
           }}
           className={
-            "ml-2 " +
+            "ml-2 select-text " +
             (phone() == "Reveal Phone Number"
               ? "p-1 px-5 ml-1 rounded-3xl bg-gray-200 hover:scale-105 hover:shadow-xl transition-all ease-in-out"
-              : "cursor-default")
+              : "cursor-text")
           }
         >
           {phone()}
@@ -34,10 +34,10 @@ export default function Contact() {
             set_email(name + "@" + domain);
           }}
           className={
-            "ml-2 " +
+            "ml-2 select-text " +
             (email() == "Reveal Email"
               ? "p-1 px-5 ml-1 rounded-3xl bg-gray-200 hover:scale-105 hover:shadow-xl transition-all ease-in-out"
-              : "cursor-default")
+              : "cursor-text")
           }
         >
           {email()}
